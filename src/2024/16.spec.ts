8f14e0f00ef5b58b31bbbaeaf5e88afcf0417816
Jakub <jjtusan@gmail.com>
2024-12-17 08:46:29 +0100

    2024 - 16 - wip

--- src/2024/16.spec.ts
+++ src/2024/16.spec.ts
@@ -1,13 +1,28 @@
 import { readFile } from 'fs/promises';
 import { join } from 'path';
 import { beforeEach, describe, expect, test } from "vitest";
-import { findAllTraces, parseMap } from "./16";
+import { Trace, draw, findAllTraces, parseMap, score, scoreRotation } from "./16";
 
 
 describe('16', () => {
 
+  describe('part one', () => {
+    test.skip('answer', async () => {
+      const input = await readFile(join(__dirname, '16.txt'), { encoding: 'utf8' });
+      const map = parseMap(input);
+
+      const traces = findAllTraces(map);
+      expect(traces.length).to.be.above(0);
+
+      const visualization = traces.map(trace => draw(trace, map));
+      const scores = traces.map(trace => score(trace));
+      scores.sort((a,b) => a-b);
+      expect(scores[0]).to.equal(0);
+    });
+  });
+
   describe('helpers', () => {
-    test('isDiagonal should return false for horizontal points', () => {
+    test('example', () => {
       const input = `
 ###############
 #.......#....E#
@@ -28,6 +43,69 @@ describe('16', () => {
 
       const traces = findAllTraces(map);
       expect(traces.length).to.be.above(0);
+
+      const visualization = traces.map(trace => draw(trace, map));
+      const scores = traces.map(trace => score(trace));
+      scores.sort((a,b) => a-b);
+      expect(scores[0]).to.equal(7036);
+    });
+
+    test('example', () => {
+      const input = `
+      #################
+      #...#...#...#..E#
+      #.#.#.#.#.#.#.#.#
+      #.#.#.#...#...#.#
+      #.#.#.#.###.#.#.#
+      #...#.#.#.....#.#
+      #.#.#.#.#.#####.#
+      #.#...#.#.#.....#
+      #.#.#####.#.###.#
+      #.#.#.......#...#
+      #.#.###.#####.###
+      #.#.#...#.....#.#
+      #.#.#.#####.###.#
+      #.#.#.........#.#
+      #.#.#.#########.#
+      #S#.............#
+      #################`;
+      const map = parseMap(input);
+
+      const traces = findAllTraces(map);
+      expect(traces.length).to.be.above(0);
+
+      const visualization = traces.map(trace => draw(trace, map));
+      const scores = traces.map(trace => score(trace));
+      scores.sort((a,b) => a-b);
+      expect(scores[0]).to.equal(11048);
+    });
+
+    test('score', () => {
+      const trace: Trace = [
+        [1,1], [2, 1], [2, 0],
+      ];
+      expect(score(trace)).to.equal(1002);
+    });
+
+    test('scoreRotation', () => {
+      const trace: Trace = [
+        [1,0], [2, 0], [2, 1],
+      ];
+      expect(scoreRotation(trace, 2)).to.equal(1000);
+    });
+
+    test('scoreRotation2', () => {
+      const trace: Trace = [
+        [1,0], [2, 0], [3, 0],
+      ];
+      expect(scoreRotation(trace, 2)).to.equal(0);
+    });
+
+    test('scoreRotation3', () => {
+      const trace: Trace = [
+        [1,1], [2, 1], [2, 0],
+      ];
+      expect(scoreRotation(trace, 2)).to.equal(1000);
     });
   });
 });
--- src/2024/16.ts
+++ src/2024/16.ts
@@ -1,11 +1,61 @@
 type Position = [number, number];
-
-type Trace = Position[];
-
-type TMap = string[][];
+export type Trace = Position[];
+export type TMap = string[][];
 
 function isOutOfBounds(position: Position, map: TMap): boolean {
   return position[0] < 0 || position[1] < 0 || position[0] >= map[0].length || position[1] >= map.length;
+}
+
+function getOrientation(prev: Position, curr: Position): Orientation {
+  if(prev[0] === curr[0] - 1 && prev[1] === curr[1]) return Orientation.East;
+  if(prev[0] === curr[0] + 1 && prev[1] === curr[1]) return Orientation.West;
+  if(prev[0] === curr[0] && prev[1] === curr[1] + 1) return Orientation.North;
+  if(prev[0] === curr[0] && prev[1] === curr[1] - 1) return Orientation.South;
+
+  throw new Error('Invalid orientation');
+}
+
+export function scoreRotation(trace: Trace, i: number): number {
+  const prev = trace[i - 1];
+  const current = trace[i];
+  const currentRotation = prev ? getOrientation(prev, current) : Orientation.East;
+  const prevRotation = i - 2 >= 0 ? getOrientation(trace[i - 2], prev) : Orientation.East;
+
+  if (currentRotation === prevRotation) return 0;
+
+  let clockWiseSteps = 0;
+  let clockWiseTest = prevRotation;
+  for(;clockWiseTest != currentRotation; clockWiseTest++, clockWiseSteps++) {
+    if ((clockWiseTest + 1) % 4 === 0) {
+      // @ts-ignore
+      clockWiseTest = -1;
+    }
+  }
+
+  let clockAntiwiseSteps = 0;
+  let clockAntiwiseTest = prevRotation;
+  for(;clockAntiwiseTest != currentRotation; clockAntiwiseTest--, clockAntiwiseSteps++) {
+    if (clockAntiwiseTest === 0) {
+      // @ts-ignore
+      clockAntiwiseTest = 4;
+    }
+  }
+
+  return Math.min(clockWiseSteps, clockAntiwiseSteps) * 1000;
+}
+
+enum Orientation { East, South, West, North };
+export function score(trace: Trace): number {
+  let sum = 0;
+
+  for(let i = 1; i < trace.length; i++) {
+    const step = 1;
+    const rotation = scoreRotation(trace, i);
+
+    sum += step + rotation;
+  }
+
+  return sum;
 }
 
 function getNextPossibleMovements(map: TMap, trace: Trace): Position[] {
@@ -40,33 +90,48 @@ function assertSamePos(p1: Position, p2: Position): boolean {
   return p1[0] === p2[0] && p1[1] === p2[1];
 }
 
+function hasBeenSeen(traces: Trace[], position: Position): boolean {
+  // Following may not be correct, e.g. if position has been seen after
+  // same amount of steps then it should be considered as good
+  return traces.some(trace => trace.some(p => assertSamePos(p, position)));
+}
+
+const MAX_STEPS = 1000;
 export function findAllTraces(map: TMap): Trace[] {
   const start = findCharacter(map, 'S');
   const end = findCharacter(map, 'E');
   const found: Position[][] = [];
   const possible: Position[][] = [[start]];
+  const toBeRemoved: Trace[] = [];
   let iteration = 0;
   do {
     const newTraces: Position[][] = [];
     for(const trace of possible) {
       const nextSteps = getNextPossibleMovements(map, trace);
-      if (nextSteps.length) {
-        const firstResult = nextSteps.pop()!;
-        nextSteps.forEach((step) => {
+      const nextNewSteps = nextSteps.filter(s => hasBeenSeen(possible, s));
+      //const nextNewSteps = nextSteps;
+
+      if (nextNewSteps.length) {
+        const firstResult = nextNewSteps.pop()!;
+        nextNewSteps.forEach((step) => {
           newTraces.push([...trace, step]);
         });
         trace.push(firstResult);
       } else {
         trace.length = 0;
+        toBeRemoved.push(trace);
       }
     }
 
     possible.push(...newTraces);
-    for(let i = 0; i < possible.length; ) {
-      if (possible[i].length === 0) {
-        possible.splice(i, 1);
-      } else i++;
+
+    // Removed blind
+    toBeRemoved.reverse();
+    for(const toRemove of toBeRemoved) {
+      const index = possible.indexOf(toRemove);
+      possible.splice(index, 1);
     }
+    toBeRemoved.length = 0;
 
 
     // Remove finished
@@ -80,11 +145,20 @@ export function findAllTraces(map: TMap): Trace[] {
         }
       }
     }
-  } while (++iteration < 100 && possible.length > 0)
+  } while (++iteration < MAX_STEPS && possible.length > 0)
 
   return found;
 }
 
 export function parseMap(input: string): TMap {
-  return input.trim().split('\n').map(row => row.split(''));
+  return input.trim().split('\n').map(row => row.trim().split(''));
+}
+
+export function draw(trace: Trace, map: TMap): TMap {
+  const result = map.map(row => row.map(c => c));
+  for(const step of trace) {
+    result[step[1]][step[0]] = 'X';
+  }
+
+  return result;
 }
